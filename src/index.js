d20d148f7b72d23f02fd4ed36b8119d7381627de
Nicholas Mulrine <Nicholas@nymbus.com.au>
2018-03-06 20:14:01 +1000

    Changed App from functional component to class based. changed apps state based on search

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from '../src/components/search_bar';
-
 const API_KEY = 'AIzaSyB-gT2awfMZbdROCAKCvLlWQADhl1bOxSw';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 // Take this component's generate HTML and put it on the page (in the DOM)
