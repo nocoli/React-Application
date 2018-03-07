98a1119f020284f03a7e6de0327c470cb10cbed6
Nicholas Mulrine <Nicholas@nymbus.com.au>
2018-03-07 22:18:43 +1000

    Finished all components. implemented more callbacks between components.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from '../src/components/search_bar';
 import VideoList from '../src/components/video_list';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 const API_KEY = 'AIzaSyB-gT2awfMZbdROCAKCvLlWQADhl1bOxSw';
 
 // Create a new component. This component should produce some HTML
@@ -10,19 +12,34 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [], 
+			selectedVideo: null
+		 };
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+		 this.videoSearch('hot wheels');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos,
+			selectedVideo: videos[0]
+			 });
 		});
 	}
 
 	// render function because its class based component
 	render() {
+		const videoSearch =_.debounce((term) => {this.videoSearch(term)}, 300);
+
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={videoSearch}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
 			</div>
 		);
 	}
